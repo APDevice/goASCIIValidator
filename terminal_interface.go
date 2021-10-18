76d629ff13836aedde863e69f9065baa1100e446
APDevice <59993100+APDevice@users.noreply.github.com>
2021-10-18 02:01:48 -0400

    added options to read from and write to file

--- terminal_interface.go
+++ terminal_interface.go
@@ -10,77 +10,171 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/APDevice/goASCIIValidator/validASCII"
 )
 
-func checkString(str string, charRange validASCII.CharRange) {
-
-}
-
-/* checkStrings: runs through all inputed strings, checks their validity, and outputs result
-
-Parameters:
-	- array of strings to parse
-	- charRange representing the ASCII standard to check against
-
-Output:
-	- one line for each string, in the format "String %: True/False"
-*/
-func checkStrings(strs []string, charRange validASCII.CharRange) {
-	var isValid bool
-	for i, str := range strs {
-		isValid = validASCII.Validate(str, charRange)
-		fmt.Printf("String %d: %t\n", i+1, isValid)
+// checkErr exits with an error if error occurs
+func checkErr(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v", err)
+		os.Exit(1)
 	}
 }
 
-/* checkStringsConcise runs through all inputed strings, checks their validity, and outputs result in a concise stream
+/* _checkStringExtended checks string and returns a string representing the validity of it
+
+Parameters:
+	- str: string to parse
+	- charRange: validASCII.CharRange representing the ASCII standard to check against
+
+Return:
+	- "valid" if code is pure ASCII, else count of invalid characters
+*/
+func _checkStringExtended(str string, charRange validASCII.CharRange) string {
+	if validASCII.Validate(str, charRange) {
+		return "valid\n"
+	}
+	return fmt.Sprintf("%d invalid characters\n", validASCII.CountNonASCII(str, charRange))
+}
+
+/* _checkStringConcise checks string and returns a byte representing the validity of it
 
 Parameters:
 	- array of strings to parse
 	- charRange representing the ASCII standard to check against
 
-Output:
-	- single line of characters, with 1 representing a valid string, 0 representing an invalid one
+Return:
+	- single byte, with 1 representing a valid string, 0 representing an invalid one
 */
-func checkStringsConcise(strs []string, charRange validASCII.CharRange) {
-	var isValid bool
-	for _, str := range strs {
-		isValid = validASCII.Validate(str, charRange)
-		if isValid {
-			fmt.Print("1")
-		} else {
-			fmt.Print("0")
+func _checkStringsConcise(str string, charRange validASCII.CharRange) byte {
+	if validASCII.Validate(str, charRange) {
+		return '1'
+	}
+	return '0'
+}
+
+/* checkStringsFromStdIN: runs through all inputed strings, checks their validity, and outputs result
+
+Parameters:
+	- strs: string array of strings to parse
+	- charRange: validASCII.CharRange representing the ASCII standard to check against
+	- consise: a bool that concise (true) and extended (false) output
+	- fileOut: string - filename for output file. If left blank, will output to STDOUT.
+
+Output (file/STDOUT):
+	- results of string check
+*/
+func checkStringsFromStdIN(strs []string, charRange validASCII.CharRange, concise bool) string {
+	var (
+		result strings.Builder
+	)
+	if concise {
+		for _, str := range strs {
+			result.WriteByte(_checkStringsConcise(str, charRange))
+		}
+		result.WriteByte('\n')
+	} else {
+		for _, str := range strs {
+			result.WriteString(_checkStringExtended(str, charRange))
 		}
 	}
-	fmt.Print("\n")
+
+	return result.String()
+}
+
+/* checkFile: runs through all characters in text file, checking their validity, and outputs result
+
+Parameters:
+	- fileIn: string - filename of input file
+	- charRange: validASCII.CharRange representing the ASCII standard to check against
+	- consise: a bool that concise (true) and extended (false) output
+	- fileOut: string - filename for output file. If left blank, will output to STDOUT.
+
+Output (file/STDOUT):
+	- results of string check
+*/
+func checkFile(fileIn string, charRange validASCII.CharRange, concise bool) string {
+	var (
+		data   []byte
+		err    error
+		result string
+	)
+	data, err = os.ReadFile(fileIn)
+	checkErr(err)
+
+	if concise {
+		result = string(_checkStringsConcise(string(data), charRange)) + "\n"
+	} else {
+		result = _checkStringExtended(string(data), charRange)
+	}
+
+	return result
+}
+
+/* outputResults prints result string to file
+
+Parameters:
+	- fileOut(string): name of file to output to
+	- result(string): string to be outputed
+
+*/
+func outputResults(fileOut string, result string) {
+	var (
+		err  error
+		fout *os.File
+	)
+
+	if len(fileOut) > 0 {
+		fout, err = os.Create(fileOut)
+		checkErr(err)
+	}
+
+	_, err = fout.WriteString(result)
+	checkErr(err)
+
+	fout.Close()
 }
 
 // Main function
 func main() {
-	checkExtended := flag.Bool("e", false, "check string against the extended ASCII range")
-	printConcise := flag.Bool("c", false, "generate a concise ouput for each string of 1 for valid, 0 for invalid")
+	var (
+		checkExtendedFlag = flag.Bool("e", false, "check string against the extended ASCII range")
+		printConciseFlag  = flag.Bool("c", false, "generate a concise ouput for each string of 1 for valid, 0 for invalid")
+		fileIn            = flag.String("r", "", "read from file")
+		fileOut           = flag.String("w", "", "write results to file")
+		result            string // store results of validator
+	)
 	flag.Parse()
-	input := flag.Args()
 
-	// check for insufficient arguments
-	if len(input) == 0 {
+	// if no strings of file given, exit with error
+	if len(flag.Args()) == 0 && len(*fileIn) == 0 {
 		fmt.Fprintf(os.Stderr, "Insufficient arguments: minimum one string needed to parse\n")
 		os.Exit(1)
 	}
 
-	if *printConcise {
-		if *checkExtended {
-			checkStringsConcise(input, validASCII.Extended)
-		} else {
-			checkStringsConcise(input, validASCII.Standard)
-		}
+	// set ascii range, defaulting to 128 unless extended flag is given
+	var asciiSet validASCII.CharRange
+	if *checkExtendedFlag {
+		asciiSet = validASCII.Extended
 	} else {
-		if *checkExtended {
-			checkStrings(input, validASCII.Extended)
-		} else {
-			checkStrings(input, validASCII.Standard)
-		}
+		asciiSet = validASCII.Standard
 	}
+
+	// check file if given, else check STDIN
+	if len(*fileIn) > 0 {
+		result = checkFile(*fileIn, asciiSet, *printConciseFlag)
+	} else {
+		stdin := flag.Args()
+		result = checkStringsFromStdIN(stdin, asciiSet, *printConciseFlag)
+	}
+
+	// output to file if given, else to STDOUT
+	if len(*fileOut) > 0 {
+		outputResults(*fileOut, result)
+	} else {
+		fmt.Print(result)
+	}
+
 }
